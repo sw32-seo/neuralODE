34c1fd512dc77c568bf88194cdb8147e799f20df
sw32-seo <sw32.seo@gmail.com>
2021-07-12 17:02:04 +0900

    class init arguments are back. Working for nfe

--- cnn_ode.py
+++ cnn_ode.py
@@ -12,43 +12,26 @@ import optax
 import tensorflow_datasets as tfds
 import numpy as np
 from tqdm import tqdm
+import os
 
 
-# Define model
-class CNN(nn.Module):
-    """A simple CNN model."""
+os.environ['CUDA_VISIBLE_DEVICES'] = '7'
 
-    @nn.compact
-    def __call__(self, inputs):
-        x = inputs
-        x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-
-        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = x.reshape((x.shape[0], -1))     # flatten
-
-        x = nn.Dense(features=256)(x)
-        x = nn.relu(x)
-        x = nn.Dense(features=10)(x)
-        x = nn.log_softmax(x)
-        return x
-
+# TODO Add system argument for dim_out, ksize, tol, learning_rate, num_epoch and batch_size
 
 # Define Residual Block
 class ResDownBlock(nn.Module):
     """Single ResBlock w/ downsample"""
+    dim_out: Any = 64
 
     @nn.compact
     def __call__(self, inputs):
         x = inputs
-        f_x = nn.relu(nn.GroupNorm(64)(x))
-        x = nn.Conv(features=64, kernel_size=(1, 1), strides=(2, 2))(x)
-        f_x = nn.Conv(features=64, kernel_size=(3, 3), strides=(2, 2))(f_x)
-        f_x = nn.relu(nn.GroupNorm(64)(f_x))
-        f_x = nn.Conv(features=64, kernel_size=(3, 3))(f_x)
+        f_x = nn.relu(nn.GroupNorm(self.dim_out)(x))
+        x = nn.Conv(features=self.dim_out, kernel_size=(1, 1), strides=(2, 2))(x)
+        f_x = nn.Conv(features=self.dim_out, kernel_size=(3, 3), strides=(2, 2))(f_x)
+        f_x = nn.relu(nn.GroupNorm(self.dim_out)(f_x))
+        f_x = nn.Conv(features=self.dim_out, kernel_size=(3, 3))(f_x)
         x = f_x + x
         return x
 
@@ -57,75 +40,82 @@ class ConcatConv2D(nn.Module):
     """Concat dynamics to hidden layer"""
     dim_out: Any = 64
     ksize: Any = 3
-    strides: Any = 1
 
     @nn.compact
     def __call__(self, x, t):
-        tt = jnp.ones_like(x[:, :, :, :1]) * t
+        tt = jnp.ones_like(x[..., :1]) * t
         ttx = jnp.concatenate([tt, x], -1)
-        return nn.Conv(features=self.dim_out, kernel_size=self.ksize, strides=self.strides)(ttx)
+        return nn.Conv(features=self.dim_out, kernel_size=self.ksize)(ttx)
 
 
 # Define Model for Mnist example in Neural ODE
 class ODEfunc(nn.Module):
     """ODE function which replace ResNet"""
-    dim: Any = 64
+    dim_out: Any = 64
+    ksize: Any = 3
 
     @nn.compact
     def __call__(self, inputs, t):
         x = inputs
-        out = nn.GroupNorm(self.dim)(x)
+        out = nn.GroupNorm(self.dim_out)(x)
         out = nn.relu(out)
-        tt = jnp.ones_like(out[..., :1]) * t
-        ttx = jnp.concatenate([tt, out], -1)
-        out = nn.Conv(features=self.dim, kernel_size=(3, 3))(ttx)
-        out = nn.GroupNorm(self.dim)(out)
+        out = ConcatConv2D(self.dim_out, self.ksize)(out, t)
+        out = nn.GroupNorm(self.dim_out)(out)
         out = nn.relu(out)
-        tt = jnp.ones_like(out[..., :1]) * t
-        ttx = jnp.concatenate([tt, out], -1)
-        out = nn.Conv(features=self.dim, kernel_size=(3, 3))(ttx)
-        out = nn.GroupNorm(self.dim)(out)
+        out = ConcatConv2D(self.dim_out, self.ksize)(out, t)
+        out = nn.GroupNorm(self.dim_out)(out)
 
         return out
 
 
 class ODEBlock(nn.Module):
     """ODE block which contains odeint"""
+    tol = 1e-3
+
     @nn.compact
     def __call__(self, x, params):
-        ode_func = ODEfunc(64, parent=None)
-        init_state, final_state = odeint(partial(ode_func.apply, {'params': params}), x, jnp.array([0., 1.]))
+        # TODO Count number of function estimation
+        is_initialized = self.has_variable('nfe', 'nfe')
+        nfe = self.variable('nfe', 'nfe', jnp.array, 0)
+        ode_func = ODEfunc(parent=None)
+        init_state, final_state = odeint(partial(ode_func.apply, {'params': params}), x, jnp.array([0., 1.]),
+                                         rtol=self.tol, atol=self.tol)
+        if is_initialized:
+            nfe.value += 1
+
         return final_state
 
 
+
 class ODEBlockVmap(nn.Module):
+    """Apply vmap to ODEBlock"""
+
     @nn.compact
     def __call__(self, x, params):
         vmap_odeblock = nn.vmap(ODEBlock,
-                                variable_axes={'params': 0},
-                                split_rngs={'params': True},
+                                variable_axes={'params': 0, 'nfe': None},
+                                split_rngs={'params': True, 'nfe': False},
                                 in_axes=(0, None))
         return vmap_odeblock(name='odeblock')(x, params)
 
 
-def odeblock(ode_func, x, ode_func_params):
-    init_state, final_state = odeint(partial(ode_func.apply, {'params': ode_func_params}), x, jnp.array([0., 1.]))
-    return final_state
+class FullODENet(nn.Module):
+    """Full ODE net which contains two downsampling layers, ODE block and linear classifier."""
+    dim_out: Any = 64
+    ksize: Any = 3
 
-
-class SmallResNet(nn.Module):
     @nn.compact
     def __call__(self, inputs):
         x = inputs
-        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+        x = nn.Conv(features=self.dim_out, kernel_size=(self.ksize, self.ksize))(x)
         x = ResDownBlock()(x)
         x = ResDownBlock()(x)
-        ode_func = ODEfunc(64)
+        ode_func = ODEfunc()
         init_fn = lambda rng, x: ode_func.init(random.split(rng)[-1], x, 0.)['params']
         ode_func_params = self.param('ode_func', init_fn, jnp.ones_like(x[0]))
         x = ODEBlockVmap()(x, ode_func_params)
 
-        x = nn.GroupNorm(64)(x)
+        x = nn.GroupNorm(self.dim_out)(x)
         x = nn.relu(x)
         x = nn.avg_pool(x, (1, 1))
 
@@ -169,38 +159,42 @@ def get_datasets():
 
 def create_train_state(rng, learning_rate):
     """Creates initial 'TrainState'."""
-    cnn = SmallResNet()
-    params = cnn.init(rng, jnp.ones([1, 28, 28, 1]))['params']
+    cnn = FullODENet()
+    variables = cnn.init(rng, jnp.ones([1, 28, 28, 1]))     #['params']
+    nfe, params = variables.pop('params')
+    del variables
     tx = optax.adam(learning_rate)
     return train_state.TrainState.create(
         apply_fn=cnn.apply, params=params, tx=tx
-    )
+    ), nfe
 
 
 # Training step
 @jax.jit
-def train_step(state, batch):
+def train_step(state, batch, nfe):
     """Train for a single step."""
-    def loss_fn(params):
-        logits = SmallResNet().apply({'params': params}, batch['image'])
+    def loss_fn(params, nfe):
+        before_logit = nfe.unfreeze()
+        logits = FullODENet().apply({'params': params, **nfe}, batch['image'])
         loss = cross_entropy_loss(logits=logits, labels=batch['label'])
+        after_logit = nfe
         return loss, logits
     grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-    (_, logits), grads = grad_fn(state.params)
+    (_, logits), grads = grad_fn(state.params, nfe)
     state = state.apply_gradients(grads=grads)
     metrics = compute_metrics(logits=logits, labels=batch['label'])
-    return state, metrics
+    return state, metrics, nfe
 
 
 # Evaluation step
 @jax.jit
 def eval_step(params, batch):
-    logits = SmallResNet().apply({'params': params}, batch['image'])
+    logits = FullODENet().apply({'params': params}, batch['image'])
     return compute_metrics(logits=logits, labels=batch['label'])
 
 
 # Train function
-def train_epoch(state, train_ds, batch_size, epoch, rng):
+def train_epoch(state, train_ds, batch_size, epoch, rng, nfe):
     """Train for a single epoch"""
     train_ds_size = len(train_ds['image'])
     steps_per_epoch = train_ds_size // batch_size
@@ -211,7 +205,7 @@ def train_epoch(state, train_ds, batch_size, epoch, rng):
     batch_metrics = []
     for perm in tqdm(perms):
         batch = {k: v[perm, ...] for k, v in train_ds.items()}
-        state, metrics = train_step(state, batch)
+        state, metrics, nfe = train_step(state, batch, nfe)
         batch_metrics.append(metrics)
 
         # compute mean of metrics across each batch in epoch.
@@ -240,17 +234,18 @@ if __name__ == '__main__':
     rng = jax.random.PRNGKey(0)
     rng, init_rng = jax.random.split(rng)
 
+    # Build learning rate decay as Neural ODE paper
     learning_rate = 0.0001
 
-    state = create_train_state(init_rng, learning_rate)
+    state, nfe = create_train_state(init_rng, learning_rate)
     del init_rng  # Must not be used anymore.
 
-    num_epochs = 1
+    num_epochs = 20
     batch_size = 128
 
     for epoch in tqdm(range(1, num_epochs + 1)):
         rng, input_rng = jax.random.split(rng)
-        state = train_epoch(state, train_ds, batch_size, epoch, input_rng)
+        state = train_epoch(state, train_ds, batch_size, epoch, input_rng, nfe)
         test_loss, test_accuracy = eval_model(state.params, test_ds)
         print(' test epoch: %d, loss: %.2f, accuracy: %.2f' % (
             epoch, test_loss, test_accuracy * 100
