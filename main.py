2c33e8daf29a6a4ae5ec5bea8d7cbd41942b7450
Seung-woo Seo <sw32.seo@gmail.com>
2021-07-13 22:31:31 +0900

    Argument help for tol is modified.

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='main.py')
     parser.add_argument("--model", type=str, choices=['odenet', 'resnet'], default="odenet", help="Type of model")
-    parser.add_argument("--tol", type=float, default=1e-1, help="Error tolerance for ODE solver")
+    parser.add_argument("--tol", type=float, default=1e-1,
+                        help="Error tolerance for ODE solver. This only works with odenet")
     parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
     parser.add_argument("--n_epoch", type=int, default=10, help="Total number of epoch")
     parser.add_argument("--batch_size", type=int, default=32, help="Number of images in batch")
